45e15f417b1348fe9f4280316ee19cfdb4f2b004
arundeshpande2000 <arundeshpande2000@gmail.com>
2025-05-01 11:39:19 +0530

    created

--- Assignment5_Task1.py
+++ Assignment5_Task1.py
@@ -1,8 +1,16 @@
-marks = {'Arun':39,'Alice':49,'Bill':83,}
+#Creating list of student name and marks
+marks = {'Arun':39,'Alice':49,'Bill':83}
 
+#Reading input from user.  Stripping the input to ensure all leading
+#and trailing spaces are ignored
 name = (input("Enter student name: ")).strip()
+
+#Capitalizing input name, to ensure it matches with names in
+#dictionary.
 name = name.capitalize()
 
+#if name exists in dictionary, print marks otherwise print
+#proper message
 if name  in marks:
     print("{}'s marks: {}".format(name,marks.get(name)))
 else:
--- Assignment5_Task2.py
+++ Assignment5_Task2.py
@@ -1,7 +1,13 @@
+#creating list of 1 to 10
 l = list(range(1,11))
 
+#slicing first five elements of list
 ll = l[:5]
+
+#revers slicing the string to reverse it.
 llr = ll[::-1]
+
+#printing output
 print("Original list: {})".format(l))
 print("Extracted list: {})".format(ll))
 print("Reverse extracted list: {})".format(llr))
